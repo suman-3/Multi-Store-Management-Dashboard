1fc36e7277dab011fee8baaff90b05e36076aa7b
Suman Mondal <mondalsumanrock20@gmail.com>
2024-05-30 19:25:55 +0530

    authentication basic page set up done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const poppins = Poppins({ subsets: ["latin"], weight: ["100", "200", "300", "400", "500","600", "700", "800", "900"]});
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
   title: "Foodied",
@@ -15,8 +19,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={poppins.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body className={poppins.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
