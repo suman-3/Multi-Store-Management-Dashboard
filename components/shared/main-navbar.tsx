235337c81bc8ac916f80b6ee892280fa4d31914f
Suman Mondal <mondalsumanrock20@gmail.com>
2024-06-01 22:24:24 +0530

    overview nam menu added

--- components/shared/main-navbar.tsx
+++ components/shared/main-navbar.tsx
@@ -13,6 +13,11 @@ export const MainNav = ({
 
   const routes = [
     {
+      href: `/${params.storeId}`,
+      label: "Overview",
+      active: pathname === `/${params.storeId}`,
+    },
+    {
       href: `/${params.storeId}/settings`,
       label: "Settings",
       active: pathname === `/${params.storeId}/settings`,
