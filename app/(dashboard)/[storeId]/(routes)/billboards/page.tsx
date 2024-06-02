3b8a4ef36a780d2f6c7b298358f95514d1c1074f
Suman Mondal <mondalsumanrock20@gmail.com>
2024-06-02 11:49:44 +0530

    billboards data fetched and logged in terminal in BillBoardsRoute page

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import { BillBoardClient } from './_components/client'
+import { BillBoardClient } from "./_components/client";
+import { collection, doc, getDocs } from "firebase/firestore";
+import { db } from "@/lib/firebase";
+import { useParams } from "next/navigation";
+import { Billboards } from "@/types-db";
 
+const BillboardsPage = async ({ params }: { params: { storeId: string } }) => {
+  const billboardsData = (
+    await getDocs(collection(doc(db, "stores", params.storeId), "billboards"))
+  ).docs.map((doc) => doc.data()) as Billboards[];
 
-
-const Billboards = () => {
+  console.log(billboardsData);
+  
   return (
-    <div className='flex flex-col'>
-        <div className='flex-1 space-y-4 p-8 pt-6'>
-            <BillBoardClient/>
-        </div>
+    <div className="flex flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <BillBoardClient />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Billboards
+export default BillboardsPage;
